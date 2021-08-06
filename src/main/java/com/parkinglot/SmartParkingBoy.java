f0df6d1ae57887b08d5c24d00742c41a1ec01c75
ORARAPA <87627137+ORARAPA@users.noreply.github.com>
2021-08-07 06:55:31 +0800

    [Story5][Paraluman] feat: implement should_return_car_parked_in_second_parking_lot_when_park_given_a_smart_parking_boy_with_first_parking_lot_has_two_parked_cars_second_lot_has_one_parked_car_and_a_car

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -1,15 +1,29 @@
 package com.parkinglot;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class SmartParkingBoy {
     private List<ParkingLot> parkingLots;
+    private ParkingLot parkingLot;
 
     public SmartParkingBoy(List<ParkingLot> parkingLots) {
         this.parkingLots = parkingLots;
     }
 
     public ParkingTicket park(Car car) {
-        return null;
+        this.parkingLot = getParkingLotWithMoreEmptyPositions(parkingLots);
+        System.out.print("Car is parked in Parking Lot " + (parkingLots.indexOf(parkingLot)+1));
+        return parkingLot.park(car);
+    }
+
+    private ParkingLot getParkingLotWithMoreEmptyPositions(List<ParkingLot> parkingLots){
+        return parkingLots
+                .stream()
+                .sorted(Comparator.comparingInt(parkingLot -> parkingLot.parkedPosition.size()))
+                .collect(Collectors.toList())
+                .get(0);
     }
 }
